0e907e0405d846614798770bb29a5239dd0ace6a
JuliianW <julian.wille.hak@gmail.com>
2016-12-17 23:06:09 +0100

    formatting, link to backend

--- index.php
+++ index.php
@@ -5,15 +5,56 @@ session_start();
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Game</title>
+        <title>Guessing Game</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <style>
+            body{
+                background: url(img/background.jpg) center center;
+                background-size: auto auto;
+            }
+            .input-group {
+                position: relative;
+                display: table;
+                border-collapse: separate;
+                padding-bottom: 10px;
+            }
+            .btn {
+                display: inline-block;
+                padding: 6px 12px;
+                margin-bottom: 0;
+                font-size: 50px;
+            }
+            input.btn-block[type="submit"] {
+                width: 100%;
+                height: 100px;
+            }
+
+            .container {
+                padding-right: 15px;
+                padding-left: 15px;
+                margin-right: auto;
+                margin-left: auto;
+                margin-top: auto;
+                padding-top: 10%;
+            }
+        </style>
     </head>
     <body>
+        <div class="container">
+            <div class="jumbotron"> 
+                <h1>Guessing Game</h1>
+                <form action="game.php" method="post">
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                        <input id="usr" type="text" class="form-control" name="usr" placeholder="Username">
+                    </div>
+                    <p>
+                        <input type="submit" class="btn btn-success btn-block" value="PLAY!">
+                    </p>
+                </form>
+            </div>
+                            <a href="login.php">Admin Login</a>
 
-        <form action="game.php" method="post">
-            <input placeholder="Username" type="text" name="usr">
-            <input type="submit" value="Play!">
-        </form>
-
-        
+        </div>
     </body>
 </html>
