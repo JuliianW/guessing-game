01b489bd499115958a3b8993676561650ffdb210
JuliianW <julian.wille.hak@gmail.com>
2016-12-17 23:06:34 +0100

    implemented counter

--- game.php
+++ game.php
@@ -5,10 +5,9 @@ session_start();
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Game</title>
+        <title>Guessing Game</title>
         <!-- Test commit! -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
     </head>
     <body>
         <?php
@@ -25,11 +24,16 @@ session_start();
         if (!isset($_SESSION['percent'])) {
             $_SESSION['percent'] = 0;
         };
+        if (!isset($counter)) {
+            $counter = 0;
+        };
+        // get $percent var
         $_SESSION['n'] = $_SESSION['wrong'] + $_SESSION['right'];
         $_SESSION['percent'] = $_SESSION['right'] / $_SESSION['n'] * 100;
+        $_SESSION['percent'] = round($_SESSION['percent'], 0);
         ?>
         <div class="container">
-            <table class="table">
+            <table class="table"> 
                 <thead>
                     <tr>
                         <th>User</th>
@@ -51,48 +55,47 @@ session_start();
                 <div class="col-sm-4">
                     <div class = "well">
                         <h3>Input</h3><hr>
-                    <h4>Your guess!</h4>
+                        <!--<h4>Your guess!</h4>-->
 
-                    <form action="#" method="post">
-                        <label>
-                            <select name="cityform" size="1">
-                                <option value="newyork">New York</option>
-                                <option value="london">London</option>
-                                <option value="paris">Paris</option>
-                            </select>
-                        </label>
-                    <input type="submit" class="btn btn-primary" value="Guess!">
-                    <input type="submit" class="btn btn-danger" name="cancel" value="Save & Exit!">
-                    </form>
-                    
+                        <form action="#" method="post">
+                            <label>
+                                <select name="cityform" size="1">
+                                    <option value="newyork">New York</option>
+                                    <option value="london">London</option>
+                                    <option value="paris">Paris</option>
+                                </select>
+                            </label>
+                            <input type="submit" class="btn btn-primary" value="Guess!">
+                            <a href="leaderboards.php" class="btn btn-danger" role="button">Save & Exit</a>
+                        </form>
+
                     </div>
 
                     <?php
-                    if(isset($_POST['cancel'])){
-                        updateBoards();
-                    }
-                    
+//                    if(isset($_POST['cancel'])){
+//                        updateBoards();
+//                    }
+
                     $cityform = $_POST['cityform'];
 
                     $servername = "localhost";
                     $username = "root";
                     $password = "root";
                     $dbname = "sightsgamedb";
-                    
+
                     // Create connection
                     $conn = new mysqli($servername, $username, $password, $dbname);
 
-                    
-                    function updateBoards(){  
-                        $conn = new mysqli($servername, $username, $password, $dbname);
-                        $sql = "INSERT INTO 'usr' ('id' , 'username', 'points', 'percentage', 'date') VALUES (NULL,'".$_SESSION['usr']."', '".$_SESSION['right']."', '".$_SESSION['percent']."', '".  date('Y-m-j')."' )";
-                        if ($conn->query($sql) === TRUE) {
-                            echo "New record created successfully";
-                        } else {
-                            echo "Error: " . $sql . "<br>" . $conn->error;
-                        }
-                    }
-                    
+//                    function updateBoards(){  
+//                        $conn = new mysqli($servername, $username, $password, $dbname);
+//                        $sql = "INSERT INTO 'usr' ('id' , 'username', 'points', 'percentage', 'date') VALUES (NULL,'".$_SESSION['usr']."', '".$_SESSION['right']."', '".$_SESSION['percent']."', '".  date('Y-m-j')."' )";
+//                        if ($conn->query($sql) === TRUE) {
+//                            echo "New record created successfully";
+//                        } else {
+//                            echo "Error: " . $sql . "<br>" . $conn->error;
+//                        }
+//                    }
+
                     function checkAnswers($cityform, $citydb) {
                         if ($cityform === $citydb) {
                             $_SESSION['right'] = $_SESSION['right'] + 1;
@@ -103,8 +106,11 @@ session_start();
                         }
                     }
 
-                    echo "Citydb: " . $_SESSION['citydb'] . "<br>";
-                    echo "Cityform: $cityform<br>";
+                    echo "<div class='well'>";
+                    echo "<h3>Your last try:</h3>";
+                    echo "<p>Right answer:<strong> " . $_SESSION['citydb'] . "</strong></p>";
+                    echo "<p>Your anser:<strong> $cityform </strong></p>";
+                    echo "</div>";
                     echo "</div>";
 
                     echo "<div class='col-sm-4'>";
@@ -112,8 +118,7 @@ session_start();
                     echo "<h3>Image</h3>";
 
                     checkAnswers($cityform, $_SESSION['citydb']);
-
-
+                    
                     //rnd img anzeigen
                     $rnd = rand(1, 3);
                     $sql1 = "SELECT * FROM sights WHERE id = $rnd";
@@ -124,33 +129,33 @@ session_start();
                     echo "<img src=" . $row1['imgpath'] . "  width='100%'>";
                     echo "</div>";
                     echo "</div>";
-                    
-                    
-                    $sql = "SELECT * FROM usr";
+
+
+                    $sql = "SELECT * FROM usr ORDER BY points DESC";
                     $result = $conn->query($sql);
-                    
-                   
                     ?>
                     <div class="col-sm-4">
                         <div class='well'>
-                        <h3>Leaderboards</h3>
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>User</th>
-                                    <th>Points</th>
-                                    <th>%</th>
-                                </tr>
-                            </thead>
-                            <tbody>
+                            <h3>Leaderboards</h3>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>User</th>
+                                        <th>Points</th>
+                                        <th>Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
                                     <?php
-                                    while($row = $result->fetch_assoc()) {
-                                        echo "<tr><td> test </td><td>" . $row["username"]. "</td><td>" . $row["points"]."</td><td>".$row['percentage']."</td></tr>";
-                                    }      ?>                           
-                                
-                           </tbody>
-                        </table>
+                                    while ($row = $result->fetch_assoc()) {
+                                        $counter = $counter + 1;
+                                        echo "<tr><td> $counter </td><td>" . $row["usr"] . "</td><td>" . $row["points"] . "</td><td>" . $row['date'] . "</td></tr>";
+                                    }
+                                    ?>                           
+
+                                </tbody>
+                            </table>
                         </div>
                     </div>    
                 </div>
